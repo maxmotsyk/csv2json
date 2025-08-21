e2dbd5cc707874435cfa7bde7706436180796e61
MacBook <21mechanic21@gmail.com>
2025-08-21 20:46:43 +0300

    set flags

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 )
 
 func main() {
-	fmt.Println("HEllo world")
+	inputFile := flag.String("in", "input.csv", "CSV file to read from")
+	outputFile := flag.String("out", "output.json", "JSON file to write to")
+	flag.Parse()
+	fmt.Println(*outputFile, *inputFile)
 }
