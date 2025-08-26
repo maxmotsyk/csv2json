b6f05e44734e86b1b7f9e1f90c116f98cf12a348
MacBook <21mechanic21@gmail.com>
2025-08-26 21:00:50 +0300

    implemented reader csv and writer to json file

--- main.go
+++ main.go
@@ -1,13 +1,86 @@
 package main
 
 import (
+	"encoding/csv"
+	"encoding/json"
 	"flag"
 	"fmt"
+	"os"
 )
+
+type CsvData struct {
+	Path    string
+	Records [][]string
+}
+
+type JsonData struct {
+	Path string
+}
+
+func (c *CsvData) GetRecords() error {
+	f, err := os.Open(c.Path)
+	if err != nil {
+		return err
+	}
+
+	defer f.Close()
+
+	reader := csv.NewReader(f)
+
+	records, err := reader.ReadAll()
+
+	if err != nil {
+		panic(err)
+	}
+
+	c.Records = records
+
+	return nil
+}
+
+func (j *JsonData) MakeRecords(csvData [][]string) error {
+	f, err := os.Create(j.Path)
+
+	if err != nil {
+		return err
+	}
+
+	defer f.Close()
+
+	encoder := json.NewEncoder(f)
+
+	csvMap := make(map[string]string, len(csvData))
+
+	for _, col := range csvData[0] {
+		csvMap[col] = ""
+
+		for _, row := range csvData[1:] {
+			for _, item := range row {
+				csvMap[col] = item
+			}
+		}
+	}
+
+	encoder.Encode(csvMap)
+
+	return nil
+}
 
 func main() {
 	inputFile := flag.String("in", "input.csv", "CSV file to read from")
 	outputFile := flag.String("out", "output.json", "JSON file to write to")
 	flag.Parse()
-	fmt.Println(*outputFile, *inputFile)
+
+	csvFile := CsvData{
+		Path: *inputFile,
+	}
+	jsonFile := JsonData{
+		Path: *outputFile,
+	}
+
+	csvFile.GetRecords()
+	jsonFile.MakeRecords(csvFile.Records)
+
+	fmt.Println(csvFile.Records)
+
 }
